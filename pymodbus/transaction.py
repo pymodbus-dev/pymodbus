8b19e3d343925df6663e525edbcc0aca28e5cfc8
Galen Collins <bashwork@gmail.com>
2009-04-27 03:59:10 +0000

    Fixing Issue:6

--- pymodbus/transaction.py
+++ pymodbus/transaction.py
@@ -317,7 +317,8 @@ class ModbusRTUFramer:
         data = message.encode()
         packet = struct.pack('>BB',
                 message.unit_id,
-                message.function_code) + data + computeCRC(data)
+                message.function_code) + data
+        packet = packet + struct.pack(">H", computeCRC(packet))
         return packet
 
 
@@ -418,5 +419,6 @@ class ModbusASCIIFramer:
         data = message.encode()
         packet = struct.pack('>BB',
                 message.unit_id,
-                message.function_code) + data + computeLRC(data)
+                message.function_code) + data
+        packet = packet + struct.pack(">H", computeLRC(packet))
         return packet

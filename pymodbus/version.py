88afe449b53c0fc721734b8e48eefe7400ed741a
Galen Collins <bashwork@gmail.com>
2010-04-17 20:26:05 +0000

    bumping the version because of bug fixes

--- pymodbus/version.py
+++ pymodbus/version.py
@@ -7,7 +7,7 @@ the svn version as well using the local .entries file.
 '''
 from twisted.python import versions
 
-_version = versions.Version('pymodbus', 0, 5, 0)
+_version = versions.Version('pymodbus', 0, 5, 1)
 _version.__name__ = "Pymodbus" # fix epydoc error
 
 #---------------------------------------------------------------------------# 

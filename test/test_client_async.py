70015c91b91e326dfa7a9628bddeb1561576e01e
Mike Pagel <mike@mpagel.de>
2015-01-24 19:40:52 +0100

    Excluded async tests due to twisted dependencies in imports. Adapted synchronous tests to new standard socket library by adding more required mocks.

--- test/test_client_async.py
+++ test/test_client_async.py
@@ -1,189 +1,195 @@
 #!/usr/bin/env python
-import unittest
-from mock import Mock
-from pymodbus.client.async import ModbusClientProtocol, ModbusUdpClientProtocol
-from pymodbus.client.async import ModbusClientFactory
-from pymodbus.exceptions import ConnectionException
-from pymodbus.exceptions import ParameterException
-from pymodbus.transaction import ModbusSocketFramer
-from pymodbus.bit_read_message import ReadCoilsRequest, ReadCoilsResponse
 
-#---------------------------------------------------------------------------#
-# Fixture
-#---------------------------------------------------------------------------#
-class AsynchronousClientTest(unittest.TestCase):
-    '''
-    This is the unittest for the pymodbus.client.async module
-    '''
+# due to implicit twisted dependency in imports these tests to not run under
+# Python 3:
+from pymodbus.compat import IS_PYTHON2
+if IS_PYTHON2:
 
-    #-----------------------------------------------------------------------#
-    # Test Client Protocol
-    #-----------------------------------------------------------------------#
+    import unittest
+    from mock import Mock
+    from pymodbus.client.async import ModbusClientProtocol, ModbusUdpClientProtocol
+    from pymodbus.client.async import ModbusClientFactory
+    from pymodbus.exceptions import ConnectionException
+    from pymodbus.exceptions import ParameterException
+    from pymodbus.transaction import ModbusSocketFramer
+    from pymodbus.bit_read_message import ReadCoilsRequest, ReadCoilsResponse
 
-    def testClientProtocolInit(self):
-        ''' Test the client protocol initialize '''
-        protocol = ModbusClientProtocol()
-        self.assertEqual(0, len(list(protocol.transaction)))
-        self.assertFalse(protocol._connected)
-        self.assertTrue(isinstance(protocol.framer, ModbusSocketFramer))
+    #---------------------------------------------------------------------------#
+    # Fixture
+    #---------------------------------------------------------------------------#
+    class AsynchronousClientTest(unittest.TestCase):
+        '''
+        This is the unittest for the pymodbus.client.async module
+        '''
 
-        framer = object()
-        protocol = ModbusClientProtocol(framer=framer)
-        self.assertEqual(0, len(list(protocol.transaction)))
-        self.assertFalse(protocol._connected)
-        self.assertTrue(framer is protocol.framer)
+        #-----------------------------------------------------------------------#
+        # Test Client Protocol
+        #-----------------------------------------------------------------------#
 
-    def testClientProtocolConnect(self):
-        ''' Test the client protocol connect '''
-        protocol = ModbusClientProtocol()
-        self.assertFalse(protocol._connected)
-        protocol.connectionMade()
-        self.assertTrue(protocol._connected)
+        def testClientProtocolInit(self):
+            ''' Test the client protocol initialize '''
+            protocol = ModbusClientProtocol()
+            self.assertEqual(0, len(list(protocol.transaction)))
+            self.assertFalse(protocol._connected)
+            self.assertTrue(isinstance(protocol.framer, ModbusSocketFramer))
 
-    def testClientProtocolDisconnect(self):
-        ''' Test the client protocol disconnect '''
-        protocol = ModbusClientProtocol()
-        protocol.connectionMade()
-        def handle_failure(failure):
-            self.assertTrue(isinstance(failure.value, ConnectionException))
-        d = protocol._buildResponse(0x00)
-        d.addErrback(handle_failure)
+            framer = object()
+            protocol = ModbusClientProtocol(framer=framer)
+            self.assertEqual(0, len(list(protocol.transaction)))
+            self.assertFalse(protocol._connected)
+            self.assertTrue(framer is protocol.framer)
 
-        self.assertTrue(protocol._connected)
-        protocol.connectionLost('because')
-        self.assertFalse(protocol._connected)
+        def testClientProtocolConnect(self):
+            ''' Test the client protocol connect '''
+            protocol = ModbusClientProtocol()
+            self.assertFalse(protocol._connected)
+            protocol.connectionMade()
+            self.assertTrue(protocol._connected)
 
-    def testClientProtocolDataReceived(self):
-        ''' Test the client protocol data received '''
-        protocol = ModbusClientProtocol()
-        protocol.connectionMade()
-        out = []
-        data = b'\x00\x00\x12\x34\x00\x06\xff\x01\x01\x02\x00\x04'
+        def testClientProtocolDisconnect(self):
+            ''' Test the client protocol disconnect '''
+            protocol = ModbusClientProtocol()
+            protocol.connectionMade()
+            def handle_failure(failure):
+                self.assertTrue(isinstance(failure.value, ConnectionException))
+            d = protocol._buildResponse(0x00)
+            d.addErrback(handle_failure)
 
-        # setup existing request
-        d = protocol._buildResponse(0x00)
-        d.addCallback(lambda v: out.append(v))
+            self.assertTrue(protocol._connected)
+            protocol.connectionLost('because')
+            self.assertFalse(protocol._connected)
 
-        protocol.dataReceived(data)
-        self.assertTrue(isinstance(out[0], ReadCoilsResponse))
+        def testClientProtocolDataReceived(self):
+            ''' Test the client protocol data received '''
+            protocol = ModbusClientProtocol()
+            protocol.connectionMade()
+            out = []
+            data = b'\x00\x00\x12\x34\x00\x06\xff\x01\x01\x02\x00\x04'
 
-    def testClientProtocolExecute(self):
-        ''' Test the client protocol execute method '''
-        protocol = ModbusClientProtocol()
-        protocol.connectionMade()
-        protocol.transport = Mock()
-        protocol.transport.write = Mock()
+            # setup existing request
+            d = protocol._buildResponse(0x00)
+            d.addCallback(lambda v: out.append(v))
 
-        request = ReadCoilsRequest(1, 1)
-        d = protocol.execute(request)
-        tid = request.transaction_id
-        self.assertEqual(d, protocol.transaction.getTransaction(tid))
+            protocol.dataReceived(data)
+            self.assertTrue(isinstance(out[0], ReadCoilsResponse))
 
-    def testClientProtocolHandleResponse(self):
-        ''' Test the client protocol handles responses '''
-        protocol = ModbusClientProtocol()
-        protocol.connectionMade()
-        out = []
-        reply = ReadCoilsRequest(1, 1)
-        reply.transaction_id = 0x00
+        def testClientProtocolExecute(self):
+            ''' Test the client protocol execute method '''
+            protocol = ModbusClientProtocol()
+            protocol.connectionMade()
+            protocol.transport = Mock()
+            protocol.transport.write = Mock()
 
-        # handle skipped cases
-        protocol._handleResponse(None)
-        protocol._handleResponse(reply)
+            request = ReadCoilsRequest(1, 1)
+            d = protocol.execute(request)
+            tid = request.transaction_id
+            self.assertEqual(d, protocol.transaction.getTransaction(tid))
 
-        # handle existing cases
-        d = protocol._buildResponse(0x00)
-        d.addCallback(lambda v: out.append(v))
-        protocol._handleResponse(reply)
-        self.assertEqual(out[0], reply)
+        def testClientProtocolHandleResponse(self):
+            ''' Test the client protocol handles responses '''
+            protocol = ModbusClientProtocol()
+            protocol.connectionMade()
+            out = []
+            reply = ReadCoilsRequest(1, 1)
+            reply.transaction_id = 0x00
 
-    def testClientProtocolBuildResponse(self):
-        ''' Test the udp client protocol builds responses '''
-        protocol = ModbusClientProtocol()
-        self.assertEqual(0, len(list(protocol.transaction)))
+            # handle skipped cases
+            protocol._handleResponse(None)
+            protocol._handleResponse(reply)
 
-        def handle_failure(failure):
-            self.assertTrue(isinstance(failure.value, ConnectionException))
-        d = protocol._buildResponse(0x00)
-        d.addErrback(handle_failure)
-        self.assertEqual(0, len(list(protocol.transaction)))
+            # handle existing cases
+            d = protocol._buildResponse(0x00)
+            d.addCallback(lambda v: out.append(v))
+            protocol._handleResponse(reply)
+            self.assertEqual(out[0], reply)
 
-        protocol._connected = True
-        d = protocol._buildResponse(0x00)
-        self.assertEqual(1, len(list(protocol.transaction)))
+        def testClientProtocolBuildResponse(self):
+            ''' Test the udp client protocol builds responses '''
+            protocol = ModbusClientProtocol()
+            self.assertEqual(0, len(list(protocol.transaction)))
 
-    #-----------------------------------------------------------------------#
-    # Test Udp Client Protocol
-    #-----------------------------------------------------------------------#
+            def handle_failure(failure):
+                self.assertTrue(isinstance(failure.value, ConnectionException))
+            d = protocol._buildResponse(0x00)
+            d.addErrback(handle_failure)
+            self.assertEqual(0, len(list(protocol.transaction)))
 
-    def testUdpClientProtocolInit(self):
-        ''' Test the udp client protocol initialize '''
-        protocol = ModbusUdpClientProtocol()
-        self.assertEqual(0, len(list(protocol.transaction)))
-        self.assertTrue(isinstance(protocol.framer, ModbusSocketFramer))
+            protocol._connected = True
+            d = protocol._buildResponse(0x00)
+            self.assertEqual(1, len(list(protocol.transaction)))
 
-        framer = object()
-        protocol = ModbusClientProtocol(framer=framer)
-        self.assertTrue(framer is protocol.framer)
+        #-----------------------------------------------------------------------#
+        # Test Udp Client Protocol
+        #-----------------------------------------------------------------------#
 
-    def testUdpClientProtocolDataReceived(self):
-        ''' Test the udp client protocol data received '''
-        protocol = ModbusUdpClientProtocol()
-        out = []
-        data = b'\x00\x00\x12\x34\x00\x06\xff\x01\x01\x02\x00\x04'
-        server = ('127.0.0.1', 12345)
+        def testUdpClientProtocolInit(self):
+            ''' Test the udp client protocol initialize '''
+            protocol = ModbusUdpClientProtocol()
+            self.assertEqual(0, len(list(protocol.transaction)))
+            self.assertTrue(isinstance(protocol.framer, ModbusSocketFramer))
 
-        # setup existing request
-        d = protocol._buildResponse(0x00)
-        d.addCallback(lambda v: out.append(v))
+            framer = object()
+            protocol = ModbusClientProtocol(framer=framer)
+            self.assertTrue(framer is protocol.framer)
 
-        protocol.datagramReceived(data, server)
-        self.assertTrue(isinstance(out[0], ReadCoilsResponse))
+        def testUdpClientProtocolDataReceived(self):
+            ''' Test the udp client protocol data received '''
+            protocol = ModbusUdpClientProtocol()
+            out = []
+            data = b'\x00\x00\x12\x34\x00\x06\xff\x01\x01\x02\x00\x04'
+            server = ('127.0.0.1', 12345)
 
-    def testUdpClientProtocolExecute(self):
-        ''' Test the udp client protocol execute method '''
-        protocol = ModbusUdpClientProtocol()
-        protocol.transport = Mock()
-        protocol.transport.write = Mock()
+            # setup existing request
+            d = protocol._buildResponse(0x00)
+            d.addCallback(lambda v: out.append(v))
 
-        request = ReadCoilsRequest(1, 1)
-        d = protocol.execute(request)
-        tid = request.transaction_id
-        self.assertEqual(d, protocol.transaction.getTransaction(tid))
+            protocol.datagramReceived(data, server)
+            self.assertTrue(isinstance(out[0], ReadCoilsResponse))
 
-    def testUdpClientProtocolHandleResponse(self):
-        ''' Test the udp client protocol handles responses '''
-        protocol = ModbusUdpClientProtocol()
-        out = []
-        reply = ReadCoilsRequest(1, 1)
-        reply.transaction_id = 0x00
+        def testUdpClientProtocolExecute(self):
+            ''' Test the udp client protocol execute method '''
+            protocol = ModbusUdpClientProtocol()
+            protocol.transport = Mock()
+            protocol.transport.write = Mock()
 
-        # handle skipped cases
-        protocol._handleResponse(None)
-        protocol._handleResponse(reply)
+            request = ReadCoilsRequest(1, 1)
+            d = protocol.execute(request)
+            tid = request.transaction_id
+            self.assertEqual(d, protocol.transaction.getTransaction(tid))
 
-        # handle existing cases
-        d = protocol._buildResponse(0x00)
-        d.addCallback(lambda v: out.append(v))
-        protocol._handleResponse(reply)
-        self.assertEqual(out[0], reply)
+        def testUdpClientProtocolHandleResponse(self):
+            ''' Test the udp client protocol handles responses '''
+            protocol = ModbusUdpClientProtocol()
+            out = []
+            reply = ReadCoilsRequest(1, 1)
+            reply.transaction_id = 0x00
 
-    def testUdpClientProtocolBuildResponse(self):
-        ''' Test the udp client protocol builds responses '''
-        protocol = ModbusUdpClientProtocol()
-        self.assertEqual(0, len(list(protocol.transaction)))
+            # handle skipped cases
+            protocol._handleResponse(None)
+            protocol._handleResponse(reply)
 
-        d = protocol._buildResponse(0x00)
-        self.assertEqual(1, len(list(protocol.transaction)))
+            # handle existing cases
+            d = protocol._buildResponse(0x00)
+            d.addCallback(lambda v: out.append(v))
+            protocol._handleResponse(reply)
+            self.assertEqual(out[0], reply)
 
-    #-----------------------------------------------------------------------#
-    # Test Client Factories
-    #-----------------------------------------------------------------------#
+        def testUdpClientProtocolBuildResponse(self):
+            ''' Test the udp client protocol builds responses '''
+            protocol = ModbusUdpClientProtocol()
+            self.assertEqual(0, len(list(protocol.transaction)))
 
-    def testModbusClientFactory(self):
-        ''' Test the base class for all the clients '''
-        factory = ModbusClientFactory()
-        self.assertTrue(factory is not None)
+            d = protocol._buildResponse(0x00)
+            self.assertEqual(1, len(list(protocol.transaction)))
+
+        #-----------------------------------------------------------------------#
+        # Test Client Factories
+        #-----------------------------------------------------------------------#
+
+        def testModbusClientFactory(self):
+            ''' Test the base class for all the clients '''
+            factory = ModbusClientFactory()
+            self.assertTrue(factory is not None)
 
 #---------------------------------------------------------------------------#
 # Main

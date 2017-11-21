1dead0df423d19fb4eb7160b0763ca546ada2fd5
rahul <rahul@riptideio.com>
2017-11-21 15:50:11 +0530

    PYM-8: Example that read from a database as a datastore

---
+++ examples/common/dbstore-update-server.py
@@ -0,0 +1,82 @@
+'''
+Pymodbus Server With Updating Thread
+--------------------------------------------------------------------------
+This is an example of having a background thread updating the
+context while the server is operating. This can also be done with
+a python thread::
+    from threading import Thread
+    thread = Thread(target=updating_writer, args=(context,))
+    thread.start()
+'''
+#---------------------------------------------------------------------------#
+# import the modbus libraries we need
+#---------------------------------------------------------------------------#
+from pymodbus.server.async import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusServerContext
+from pymodbus.datastore.database import
+from pymodbus.datastore.database import DatabaseSlaveContext
+from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
+
+#---------------------------------------------------------------------------#
+# import the twisted libraries we need
+#---------------------------------------------------------------------------#
+from twisted.internet.task import LoopingCall
+
+#---------------------------------------------------------------------------#
+# configure the service logging
+#---------------------------------------------------------------------------#
+import logging
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+#---------------------------------------------------------------------------#
+# define your callback process
+#---------------------------------------------------------------------------#
+def updating_writer(a):
+    ''' A worker process that runs every so often and
+    updates live values of the context. It should be noted
+    that there is a race condition for the update.
+    :param arguments: The input arguments to the call
+    '''
+    log.debug("updating the context")
+    context  = a[0]
+    readfunction = 0x03 # read holding registers
+    writefunction = 0x10
+    slave_id = 0x01 # slave address
+    address  = 16 # adress : 400017
+
+
+    values = context[slave_id].getValues(readfunction, address, count=3)
+    log.debug("new values: " + str(values))
+
+
+#---------------------------------------------------------------------------#
+# initialize your data store
+#---------------------------------------------------------------------------#
+block = ModbusSequentialDataBlock(0x00, [0]*0xff)
+store = DatabaseSlaveContext(block)
+
+context = ModbusServerContext(slaves={1: store}, single=False)
+
+
+#---------------------------------------------------------------------------#
+# initialize the server information
+#---------------------------------------------------------------------------#
+identity = ModbusDeviceIdentification()
+identity.VendorName  = 'pymodbus'
+identity.ProductCode = 'PM'
+identity.VendorUrl   = 'http://github.com/bashwork/pymodbus/'
+identity.ProductName = 'pymodbus Server'
+identity.ModelName   = 'pymodbus Server'
+identity.MajorMinorRevision = '1.0'
+
+#---------------------------------------------------------------------------#
+# run the server you want
+#---------------------------------------------------------------------------#
+time = 5 # 5 seconds delay
+loop = LoopingCall(f=updating_writer, a=(context,))
+loop.start(time, now=False) # initially delay by time
+StartTcpServer(context, identity=identity, address=("", 5020))

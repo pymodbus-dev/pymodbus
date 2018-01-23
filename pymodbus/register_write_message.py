08137603ea613e3ad65146d6991b4f68d2631913
yy123xiang <yy123xiang@163.com>
2018-01-23 11:40:40 +0800

    Add get_response_pdu_size to WriteMultipleRegistersRequest

--- pymodbus/register_write_message.py
+++ pymodbus/register_write_message.py
@@ -192,6 +192,13 @@ class WriteMultipleRegistersRequest(ModbusRequest):
         context.setValues(self.function_code, self.address, self.values)
         return WriteMultipleRegistersResponse(self.address, self.count)
 
+    def get_response_pdu_size(self):
+        """
+        Func_code (1 byte) + Register Address(2 byte) + Quantity of Registers (2 bytes)
+        :return: 
+        """
+        return 1 + 2 + 2
+
     def __str__(self):
         ''' Returns a string representation of the instance
 

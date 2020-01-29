ea4705e0e961fdf383bc03a56d8fc21545fef0bb
sanjay <sanjay.kv@atherenergy.com>
2020-01-29 09:01:17 +0530

    PR #471 Fix transaction tests

--- pymodbus/transaction.py
+++ pymodbus/transaction.py
@@ -228,7 +228,7 @@ class ModbusTransactionManager(object):
                 _logger.debug("Changing transaction state from 'SENDING' "
                               "to 'WAITING FOR REPLY'")
                 self.client.state = ModbusTransactionState.WAITING_FOR_REPLY
-            if self.client.handle_local_echo:
+            if self.client.handle_local_echo is True:
                 local_echo_packet = self._recv(size, full)
                 if local_echo_packet != packet:
                     return b'', "Wrong local echo"

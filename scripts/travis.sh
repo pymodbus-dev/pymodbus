d585a2f29456b2fc2d2b5ed9c5f87e08218900d5
dhoomakethu <otlasanju@gmail.com>
2020-09-10 09:13:22 +0530

    Osx travis issue - Fix trial 1

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 
-#if [ "$TRAVIS_OS_NAME" = osx ]; then
-#  VIRTUAL_ENV="$HOME/.virtualenvs/python2.7"
+if [ "$TRAVIS_OS_NAME" = osx ]; then
+  VIRTUAL_ENV="$HOME/.virtualenvs/pymodbus"
+  python3 -m venv $VIRTUAL_ENV
 #  if [ ! -x "$VIRTUAL_ENV/bin/python" ]; then
 #    virtualenv "$VIRTUAL_ENV"
 #  fi
-#  source "$VIRTUAL_ENV/bin/activate"
-#fi
+  source "$VIRTUAL_ENV/bin/activate"
+fi
 
 eval "$@"

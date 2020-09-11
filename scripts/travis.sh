85a04d6e87e72f38dafade7e690bc14189aa3c7c
dhoomakethu <otlasanju@gmail.com>
2020-09-11 12:58:52 +0530

    Travis reverting xcode to 8.x for mac osx

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
-
+set -x
 if [ "$TRAVIS_OS_NAME" = osx ]; then
-  VIRTUAL_ENV="$HOME/.virtualenvs/pymodbus"
-  python3 -m venv $VIRTUAL_ENV
-#  if [ ! -x "$VIRTUAL_ENV/bin/python" ]; then
-#    virtualenv "$VIRTUAL_ENV"
-#  fi
+  VIRTUAL_ENV="$HOME/.virtualenvs/python2.7"
+  if [ ! -x "$VIRTUAL_ENV/bin/python" ]; then
+    virtualenv "$VIRTUAL_ENV"
+  fi
   source "$VIRTUAL_ENV/bin/activate"
 fi
 

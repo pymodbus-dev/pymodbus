d484f47382f6c16c8e6ffca3dafb3d63af4b052a
Fabian Beitler <f.beitler@konzeptpark.de>
2020-10-16 09:27:42 +0200

    Drop python2 from tox file Drop python 2 from travis.sh Try fixing osx to get python installed...

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e
 set -x
 if [ "$TRAVIS_OS_NAME" = osx ]; then
-  VIRTUAL_ENV="$HOME/.virtualenvs/python2.7"
-  if [ ! -x "$VIRTUAL_ENV/bin/python" ]; then
-    virtualenv "$VIRTUAL_ENV"
-  fi
+  virtualenv "$VIRTUAL_ENV"
   source "$VIRTUAL_ENV/bin/activate"
 fi
 

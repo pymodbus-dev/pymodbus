d75533255e9bada24caf98e08efc726976e8ccc9
Kyle Altendorf <sda@fstab.net>
2021-01-16 10:31:44 -0500

    adjust py2 exclusions

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,10 @@
-from pymodbus.compat import IS_PYTHON3, PYTHON_VERSION
-if not IS_PYTHON3 or IS_PYTHON3 and PYTHON_VERSION.minor < 7:
-    collect_ignore = ["test_server_asyncio.py"]
+from pymodbus.compat import PYTHON_VERSION
+
+if PYTHON_VERSION < (3,):
+    collect_ignore = [
+        # TODO: do these really need to be ignored on py2 or can they just get
+        #       super() etc fixed?
+        "test_client_async.py",
+        "test_client_async_tornado.py",
+        "test_server_asyncio.py",
+    ]

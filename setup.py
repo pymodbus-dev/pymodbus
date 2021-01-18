5fbe39900078f7dacd1a255edf0d0d9555992d7f
Kyle Altendorf <sda@fstab.net>
2021-01-18 07:47:10 -0500

    Document 2.7, 3.6-3.9 support in classifiers and python_requires

--- setup.py
+++ setup.py
@@ -47,6 +47,13 @@ setup(
     """,
     classifiers=[
         'Development Status :: 4 - Beta',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         'Environment :: Console',
         'Environment :: X11 Applications :: GTK',
         'Framework :: Twisted',
@@ -71,6 +78,7 @@ setup(
     platforms=['Linux', 'Mac OS X', 'Win'],
     include_package_data=True,
     zip_safe=True,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     install_requires=install_requires,
     extras_require={
         'quality': [

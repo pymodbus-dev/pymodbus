ba6814ac49314a334a34bf127c52093a531b42c7
Kyle Altendorf <kyle.altendorf@epcpower.com>
2021-01-19 10:31:33 -0800

    some more sleeps to help asyncio tests

--- test/test_server_asyncio.py
+++ test/test_server_asyncio.py
@@ -211,7 +211,9 @@ class AsyncioServerTest(asynctest.TestCase):
 
         transport, protocol = yield from self.loop.create_connection(BasicClient, host='127.0.0.1', port=random_port)
         yield from step1
-        # await asyncio.sleep(1)
+        # On Windows we seem to need to give this an extra chance to finish,
+        # otherwise there ends up being an active connection at the assert.
+        yield from asyncio.sleep(0.0)
         self.assertTrue(len(server.active_connections) == 1)
 
         protocol.transport.close()  # close isn't synchronous and there's no notification that it's done
@@ -246,6 +248,9 @@ class AsyncioServerTest(asynctest.TestCase):
         transport, protocol = yield from self.loop.create_connection(BasicClient, host='127.0.0.1',port=random_port)
         yield from step1
 
+        # On Windows we seem to need to give this an extra chance to finish,
+        # otherwise there ends up being an active connection at the assert.
+        yield from asyncio.sleep(0.0)
         server.server_close()
 
         # close isn't synchronous and there's no notification that it's done

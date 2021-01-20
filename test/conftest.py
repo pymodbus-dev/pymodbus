35e1468949e74dad4480ad7847b63723f2136e4d
Kyle Altendorf <sda@fstab.net>
2021-01-20 09:27:27 -0500

    touchup conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,5 @@
 from pymodbus.compat import PYTHON_VERSION
-
 if PYTHON_VERSION < (3,):
-    # These files use syntax introduced in Python 3 (not necessarily 3.0) and
-    # just won't be run during tests in Python 2.
-    collect_ignore = [
-        "test_server_asyncio.py",
-    ]
+    # These files use syntax introduced between Python 2 and our lowest
+    # supported Python 3 version.  We just won't run these tests in Python 2.
+    collect_ignore = ["test_server_asyncio.py"]

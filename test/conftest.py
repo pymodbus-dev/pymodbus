e9309464f070cd580d0849175a6ef6d67b6c3316
Kyle Altendorf <kyle.altendorf@epcpower.com>
2021-01-19 20:18:55 -0800

    un-ignore a couple test files

--- test/conftest.py
+++ test/conftest.py
@@ -2,9 +2,5 @@ from pymodbus.compat import PYTHON_VERSION
 
 if PYTHON_VERSION < (3,):
     collect_ignore = [
-        # TODO: do these really need to be ignored on py2 or can they just get
-        #       super() etc fixed?
-        "test_client_async.py",
-        "test_client_async_tornado.py",
         "test_server_asyncio.py",
     ]

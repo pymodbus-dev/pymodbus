325a53e41466fdaa420b45bebe538117ab8a69b9
Kyle Altendorf <sda@fstab.net>
2021-04-23 12:38:34 -0400

    unit test trio.execute()

--- test/test_server_trio.py
+++ test/test_server_trio.py
@@ -10,7 +10,7 @@ from pymodbus.datastore.context import ModbusServerContext, ModbusSlaveContext
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.pdu import ExceptionResponse, ModbusExceptions
 from pymodbus.register_read_message import ReadHoldingRegistersResponse
-from pymodbus.server.trio import tcp_server
+from pymodbus.server.trio import execute, tcp_server
 from pymodbus.register_write_message import WriteMultipleRegistersResponse
 
 
@@ -22,7 +22,7 @@ class RunningTrioServer:
 
 
 @pytest.fixture(name="trio_server_context")
-async def trio_server_context_fixture(request):
+def trio_server_context_fixture(request):
     node = request.node
     single = node.get_closest_marker("single")
     no_contexts = node.get_closest_marker("no_contexts")
@@ -38,6 +38,15 @@ async def trio_server_context_fixture(request):
             slaves = {0: slave_context}
 
     server_context = ModbusServerContext(slaves=slaves, single=single)
+
+    return server_context
+
+
+@pytest.fixture(name="trio_server_multiunit_context")
+def trio_server_multiunit_context_fixture(request):
+    slaves = {i: ModbusSlaveContext() for i in range(3)}
+
+    server_context = ModbusServerContext(slaves=slaves, single=False)
 
     return server_context
 
@@ -187,3 +196,126 @@ async def test_logs_server_response_send(trio_tcp_client, caplog):
         await trio_tcp_client.read_holding_registers(address=0, count=1)
 
     assert "send: [ReadHoldingRegistersResponse (1)]- b'0001000000050003020000'" in caplog.text
+
+
+class Response:
+    def __init__(self):
+        self.transaction_id = None
+        self.unit_id = None
+
+
+class Request:
+    def __init__(self, unit_id, transaction_id=0, fail_to_execute=False):
+        self.exception_codes = []
+        self.executed_contexts = []
+        self.fail_to_execute = fail_to_execute
+        self.transaction_id = transaction_id
+        self.unit_id = unit_id
+
+    def execute(self, context):
+        if self.fail_to_execute:
+            raise Exception('failing to execute for testing purposes')
+        self.executed_contexts.append(context)
+        return Response()
+
+    def doException(self, exception):
+        self.exception_codes.append(exception)
+        return Response()
+
+
+def test_execute_broadcasts(trio_server_multiunit_context):
+    context = trio_server_multiunit_context
+
+    test_request = Request(unit_id=0)
+    execute(
+        request=test_request,
+        addr=None,
+        context=context,
+        response_send=None,
+        ignore_missing_slaves=False,
+        broadcast_enable=True,
+    )
+
+    assert len(test_request.executed_contexts) == len(context.slaves())
+    assert test_request.exception_codes == []
+
+
+@pytest.mark.parametrize(
+    argnames=["unit_id", "broadcast_enabled"],
+    argvalues=[[0, False], [1, True]],
+)
+def test_execute_does_not_broadcast(
+    trio_server_multiunit_context,
+    unit_id,
+    broadcast_enabled,
+):
+    context = trio_server_multiunit_context
+    response_send, response_receive = trio.open_memory_channel(max_buffer_size=1)
+
+    test_request = Request(unit_id=unit_id)
+    execute(
+        request=test_request,
+        addr=None,
+        context=context,
+        response_send=response_send,
+        ignore_missing_slaves=False,
+        broadcast_enable=broadcast_enabled,
+    )
+
+    assert len(test_request.executed_contexts) == 1
+    assert test_request.exception_codes == []
+
+
+def test_execute_does_ignore_missing_slaves(trio_server_multiunit_context):
+    context = trio_server_multiunit_context
+
+    missing_unit_id = max(context.slaves()) + 1
+
+    test_request = Request(unit_id=missing_unit_id)
+    execute(
+        request=test_request,
+        addr=None,
+        context=context,
+        response_send=None,
+        ignore_missing_slaves=True,
+        broadcast_enable=True,
+    )
+
+    assert len(test_request.executed_contexts) == 0
+    assert test_request.exception_codes == []
+
+
+def test_execute_does_not_ignore_missing_slaves(trio_server_multiunit_context):
+    context = trio_server_multiunit_context
+    response_send, response_receive = trio.open_memory_channel(max_buffer_size=1)
+
+    missing_unit_id = max(context.slaves()) + 1
+
+    test_request = Request(unit_id=missing_unit_id)
+    execute(
+        request=test_request,
+        addr=None,
+        context=context,
+        response_send=response_send,
+        ignore_missing_slaves=False,
+        broadcast_enable=True,
+    )
+
+    assert test_request.exception_codes == [ModbusExceptions.GatewayNoResponse]
+
+
+def test_execute_handles_slave_failure(trio_server_multiunit_context):
+    context = trio_server_multiunit_context
+    response_send, response_receive = trio.open_memory_channel(max_buffer_size=1)
+
+    test_request = Request(unit_id=0, fail_to_execute=True)
+    execute(
+        request=test_request,
+        addr=None,
+        context=context,
+        response_send=response_send,
+        ignore_missing_slaves=False,
+        broadcast_enable=True,
+    )
+
+    assert test_request.exception_codes == [ModbusExceptions.SlaveFailure]

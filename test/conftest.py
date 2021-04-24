6f1af2faa1f15ad5badbc90319fd51d965ffe252
Kyle Altendorf <sda@fstab.net>
2021-04-24 16:57:52 -0400

    simplify trio client test file

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,17 @@
 from pymodbus.compat import PYTHON_VERSION
+
+collect_ignore = []
+
 if PYTHON_VERSION < (3,):
     # These files use syntax introduced between Python 2 and our lowest
     # supported Python 3 version.  We just won't run these tests in Python 2.
-    collect_ignore = [
+    collect_ignore.extend([
         "test_client_async_asyncio.py",
         "test_server_asyncio.py",
+    ])
+
+if PYTHON_VERSION < (3, 6):
+    collect_ignore.extend([
+        "test_client_async_trio.py",
         "test_server_trio.py",
-    ]
+    ])

33e11e1a4d8319ae5204b29ef9e8068b08ac599b
jan Iversen <jancasacondor@gmail.com>
2022-04-07 12:55:44 +0200

    Pylint 4.

--- pymodbus/client/asynchronous/factory/tls.py
+++ pymodbus/client/asynchronous/factory/tls.py
@@ -3,14 +3,14 @@ Factory to create asynchronous tls clients based on asyncio
 """
 from __future__ import unicode_literals
 from __future__ import absolute_import
-
 import logging
+import asyncio
 
 from pymodbus.client.asynchronous import schedulers
-from pymodbus.client.asynchronous.thread import EventLoopThread
+from pymodbus.client.asynchronous.async_io import init_tls_client
 from pymodbus.constants import Defaults
 
-LOGGER = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 def async_io_factory(host="127.0.0.1", port=Defaults.TLSPort, sslctx=None,
                      server_hostname=None, framer=None, **kwargs):
@@ -28,8 +28,6 @@ def async_io_factory(host="127.0.0.1", port=Defaults.TLSPort, sslctx=None,
     :param kwargs:
     :return: asyncio event loop and tcp client
     """
-    import asyncio
-    from pymodbus.client.asynchronous.async_io import init_tls_client
 
     try:
         loop = kwargs.pop("loop", None) or asyncio.get_event_loop()
@@ -62,8 +60,8 @@ def get_factory(scheduler):
     """
     if scheduler == schedulers.ASYNC_IO:
         return async_io_factory
-    else:
-        LOGGER.warning("Allowed Schedulers: {}".format(
-            schedulers.ASYNC_IO
-        ))
-        raise Exception("Invalid Scheduler '{}'".format(scheduler))
+
+    txt = f"Allowed Schedulers: {schedulers.ASYNC_IO}"
+    _logger.warning(txt)
+    txt = f"Invalid Scheduler '{scheduler}'"
+    raise Exception(txt) #NOSONAR
--- pymodbus/client/asynchronous/factory/udp.py
+++ pymodbus/client/asynchronous/factory/udp.py
@@ -1,13 +1,15 @@
+"""UDP implementation."""
 from __future__ import unicode_literals
 from __future__ import absolute_import
-
 import logging
+import asyncio
 
+from pymodbus.client.asynchronous.async_io import init_udp_client
 from pymodbus.client.asynchronous import schedulers
 from pymodbus.client.asynchronous.thread import EventLoopThread
 from pymodbus.constants import Defaults
 
-LOGGER = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 
 def reactor_factory(host="127.0.0.1", port=Defaults.Port, framer=None,
@@ -37,9 +39,9 @@ def io_loop_factory(host="127.0.0.1", port=Defaults.Port, framer=None,
     :param kwargs:
     :return: event_loop_thread and tornado future
     """
-    from tornado.ioloop import IOLoop
+    from tornado.ioloop import IOLoop # pylint: disable=import-outside-toplevel
     from pymodbus.client.asynchronous.tornado import AsyncModbusUDPClient as \
-        Client
+        Client # pylint: disable=import-outside-toplevel
 
     client = Client(host=host, port=port, framer=framer,
                     source_address=source_address,
@@ -63,8 +65,6 @@ def async_io_factory(host="127.0.0.1", port=Defaults.Port, **kwargs):
     :param kwargs:
     :return: asyncio event loop and udp client
     """
-    import asyncio
-    from pymodbus.client.asynchronous.async_io import init_udp_client
     try:
         loop = kwargs.pop("loop", None) or asyncio.get_event_loop()
     except RuntimeError:
@@ -77,10 +77,10 @@ def async_io_factory(host="127.0.0.1", port=Defaults.Port, **kwargs):
         client = loop.run_until_complete(asyncio.gather(cor))[0]
     elif loop is asyncio.get_event_loop():
         return loop, init_udp_client(proto_cls, loop, host, port)
-    else:
-        cor = init_udp_client(proto_cls, loop, host, port)
-        client = asyncio.run_coroutine_threadsafe(cor, loop=loop)
-        client = client.result()
+
+    cor = init_udp_client(proto_cls, loop, host, port)
+    client = asyncio.run_coroutine_threadsafe(cor, loop=loop)
+    client = client.result()
 
     return loop, client
 
@@ -93,13 +93,12 @@ def get_factory(scheduler):
     """
     if scheduler == schedulers.REACTOR:
         return reactor_factory
-    elif scheduler == schedulers.IO_LOOP:
+    if scheduler == schedulers.IO_LOOP:
         return io_loop_factory
-    elif scheduler == schedulers.ASYNC_IO:
+    if scheduler == schedulers.ASYNC_IO:
         return async_io_factory
-    else:
-        LOGGER.warning("Allowed Schedulers: {}, {}, {}".format(
-            schedulers.REACTOR, schedulers.IO_LOOP, schedulers.ASYNC_IO
-        ))
-        raise Exception("Invalid Scheduler '{}'".format(scheduler))
 
+    txt = f"Allowed Schedulers: {schedulers.REACTOR}, {schedulers.IO_LOOP}, {schedulers.ASYNC_IO}"
+    _logger.warning(txt)
+    txt = f"Invalid Scheduler '{scheduler}'"
+    raise Exception(txt)

9f5ff898237b97e773e666de5db4d21255e9c563
jan Iversen <jancasacondor@gmail.com>
2022-04-11 15:52:58 +0200

    Pylint 11.

--- pymodbus/__init__.py
+++ pymodbus/__init__.py
@@ -11,6 +11,7 @@ TwistedModbus is built on top of the code developed by:
 Released under the the BSD license
 '''
 
+import logging as __logging
 import pymodbus.version as __version
 __version__ = __version.version.short()
 __author__  = 'Galen Collins'
@@ -19,13 +20,11 @@ __maintainer__ = 'dhoomakethu'
 #---------------------------------------------------------------------------#
 # Block unhandled logging
 #---------------------------------------------------------------------------#
-import logging as __logging
 try:
     from logging import NullHandler as __null
 except ImportError:
-    class __null(__logging.Handler):
+    class __null(__logging.Handler): # pylint: disable=invalid-name
         def emit(self, record):
             pass
 
 __logging.getLogger(__name__).addHandler(__null())
-

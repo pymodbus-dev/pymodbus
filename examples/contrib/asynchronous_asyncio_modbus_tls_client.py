4b433ab96992f337be800a3983b8bd6e91329866
jan Iversen <jancasacondor@gmail.com>
2022-04-20 18:00:12 +0200

    Pylint 27.

--- examples/contrib/asynchronous_asyncio_modbus_tls_client.py
+++ examples/contrib/asynchronous_asyncio_modbus_tls_client.py
@@ -27,6 +27,7 @@ sslctx.check_hostname = True
 
 
 async def start_async_test(client):
+    """ Start async test. """
     result = await client.read_coils(1, 8)
     print(result.bits)
     await client.write_coils(1, [False] * 3)
@@ -37,7 +38,7 @@ if __name__ == '__main__':
     # ----------------------------------------------------------------------- #
     # pass SSLContext which is the context here to ModbusTcpClient()
     # ----------------------------------------------------------------------- #
-    loop, client = AsyncModbusTLSClient(ASYNC_IO, 'test.host.com', 8020,
+    loop, new_client = AsyncModbusTLSClient(ASYNC_IO, 'test.host.com', 8020, #NOSONAR pylint: disable=unpacking-non-sequence
                                         sslctx=sslctx)
-    loop.run_until_complete(start_async_test(client.protocol))
+    loop.run_until_complete(start_async_test(new_client.protocol))
     loop.close()

65dcdd18cb5492a2e8a0a9edfa125933c5890f6b
jan Iversen <jancasacondor@gmail.com>
2022-04-21 12:21:40 +0200

    Pylint cleanup.

--- examples/common/custom_synchronous_server.py
+++ examples/common/custom_synchronous_server.py
@@ -62,7 +62,7 @@ import logging
 # --------------------------------------------------------------------------- #
 # import the various server implementations
 # --------------------------------------------------------------------------- #
-from custom_message import CustomModbusRequest
+from custom_message import CustomModbusRequest # pylint: disable=import-error
 
 from pymodbus.version import version
 from pymodbus.server.sync import StartTcpServer

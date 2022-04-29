8b24cf8592ce70c6ff98c6caac9a9e54b841f9e5
jan Iversen <jancasacondor@gmail.com>
2022-04-29 09:17:39 +0200

    Revert "Pylint 19. (#780)"
    This reverts commit 6a99bea827d680161b7a46a68b9d1a34cc5e590f.

--- test/test_client_common.py
+++ test/test_client_common.py
@@ -1,41 +1,24 @@
 #!/usr/bin/env python3
-""" Test client common. """
 import unittest
 from pymodbus.client.common import ModbusClientMixin
-from pymodbus.bit_read_message import (
-    ReadDiscreteInputsRequest,
-    ReadCoilsRequest,
-)
-from pymodbus.bit_write_message import (
-    WriteMultipleCoilsRequest,
-    WriteSingleCoilRequest,
-)
-from pymodbus.register_read_message import (
-    ReadWriteMultipleRegistersRequest,
-    ReadInputRegistersRequest,
-    ReadHoldingRegistersRequest,
-)
-from pymodbus.register_write_message import (
-    MaskWriteRegisterRequest,
-    WriteMultipleRegistersRequest,
-    WriteSingleRegisterRequest,
-)
+from pymodbus.bit_read_message import *
+from pymodbus.bit_write_message import *
+from pymodbus.file_message import *
+from pymodbus.register_read_message import *
+from pymodbus.register_write_message import *
 
 #---------------------------------------------------------------------------#
 # Mocks
 #---------------------------------------------------------------------------#
 class MockClient(ModbusClientMixin):
-    """ Mock client. """
 
-    def execute(self, request): # pylint: disable=no-self-use
-        """ Mock execute. """
+    def execute(self, request):
         return request
 
 #---------------------------------------------------------------------------#
 # Fixture
 #---------------------------------------------------------------------------#
 class ModbusCommonClientTests(unittest.TestCase):
-    """ Modbus common client tests. """
 
     #-----------------------------------------------------------------------#
     # Setup/TearDown
@@ -54,30 +37,19 @@ class ModbusCommonClientTests(unittest.TestCase):
     #-----------------------------------------------------------------------#
     # Tests
     #-----------------------------------------------------------------------#
-    def test_modbus_client_mixin_methods(self):
+    def testModbusClientMixinMethods(self):
         ''' This tests that the mixing returns the correct request object '''
         arguments = {
             'read_address': 1, 'read_count': 1,
             'write_address': 1, 'write_registers': 1
         }
-        self.assertTrue(
-            isinstance(self.client.read_coils(1,1), ReadCoilsRequest))
-        self.assertTrue(
-            isinstance(self.client.read_discrete_inputs(1,1), ReadDiscreteInputsRequest))
-        self.assertTrue(
-            isinstance(self.client.write_coil(1,True), WriteSingleCoilRequest))
-        self.assertTrue(
-            isinstance(self.client.write_coils(1,[True]), WriteMultipleCoilsRequest))
-        self.assertTrue(
-            isinstance(self.client.write_register(1,0x00), WriteSingleRegisterRequest))
-        self.assertTrue(
-            isinstance(self.client.write_registers(1,[0x00]), WriteMultipleRegistersRequest))
-        self.assertTrue(
-            isinstance(self.client.read_holding_registers(1,1), ReadHoldingRegistersRequest))
-        self.assertTrue(
-            isinstance(self.client.read_input_registers(1,1), ReadInputRegistersRequest))
-        self.assertTrue(
-            isinstance(self.client.readwrite_registers(**arguments),
-                ReadWriteMultipleRegistersRequest))
-        self.assertTrue(
-            isinstance(self.client.mask_write_register(1,0,0), MaskWriteRegisterRequest))
+        self.assertTrue(isinstance(self.client.read_coils(1,1), ReadCoilsRequest))
+        self.assertTrue(isinstance(self.client.read_discrete_inputs(1,1), ReadDiscreteInputsRequest))
+        self.assertTrue(isinstance(self.client.write_coil(1,True), WriteSingleCoilRequest))
+        self.assertTrue(isinstance(self.client.write_coils(1,[True]), WriteMultipleCoilsRequest))
+        self.assertTrue(isinstance(self.client.write_register(1,0x00), WriteSingleRegisterRequest))
+        self.assertTrue(isinstance(self.client.write_registers(1,[0x00]), WriteMultipleRegistersRequest))
+        self.assertTrue(isinstance(self.client.read_holding_registers(1,1), ReadHoldingRegistersRequest))
+        self.assertTrue(isinstance(self.client.read_input_registers(1,1), ReadInputRegistersRequest))
+        self.assertTrue(isinstance(self.client.readwrite_registers(**arguments), ReadWriteMultipleRegistersRequest))
+        self.assertTrue(isinstance(self.client.mask_write_register(1,0,0), MaskWriteRegisterRequest))

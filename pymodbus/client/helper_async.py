3bd6200a19a3180993ffc9a2fe9d6349bb81031f
jan Iversen <jancasacondor@gmail.com>
2022-08-18 12:16:58 +0200

    Revert "Combine ModbusClientProtocol and ModbusUdpClientProtocol."
    This reverts commit 9b1ae43dcbba38d19142c120068c0b0eb7991331.

--- pymodbus/client/helper_async.py
+++ pymodbus/client/helper_async.py
@@ -25,27 +25,28 @@ class ModbusClientProtocol(
     #: Factory that created this instance.
     factory = None
     transport = None
+    use_udp = False
 
     def __init__(
         self,
-        host="127.0.0.1",
-        port=502,
         source_address=None,
         use_udp=False,
         **kwargs
     ):
-        """Initialize a Modbus TCP/UDP asynchronous client"""
-        super().__init__(**kwargs)
+        """Initialize a Modbus TCP/UDP asynchronous client
+
+        :param host: Host IP address
+        :param port: Port
+        :param framer: Framer to use
+        :param source_address: Specific to underlying client being used
+        :param timeout: Timeout in seconds
+        :param kwargs: Extra arguments
+        """
         self.use_udp = use_udp
-        self.params.host = host
-        self.params.port = port
-        self.params.source_address = source_address or ("", 0)
-
         self._connected = False
+        super().__init__(**kwargs)
 
-    def datagram_received(self, data, addr):
-        """Receive datagram."""
-        self._data_received(data)
+        self.source_address = source_address or ("", 0)
 
     async def execute(self, request=None):  # pylint: disable=invalid-overridden-method
         """Execute requests asynchronously.

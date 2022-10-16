a82f30e960e81ede3c5924a8660ce4b7db1f5745
Alexandre CUER <alexandre.cuer@cerema.fr>
2022-10-16 16:53:15 +0200

    solving pylint errors

--- examples/serial_forwarder.py
+++ examples/serial_forwarder.py
@@ -1,5 +1,4 @@
-"""
-Pymodbus SerialRTU2TCP Forwarder
+"""Pymodbus SerialRTU2TCP Forwarder
 
 usage :
 python3 serial_forwarder.py --log DEBUG --port "/dev/ttyUSB0" --baudrate 9600 --server_ip "192.168.1.27" --server_port 5020 --slaves 1 2 3
@@ -21,26 +20,34 @@ logging.basicConfig(format=FORMAT)
 _logger = logging.getLogger()
 
 def raise_graceful_exit(*args):
-   _logger.info("receiving shutdown signal now")
-   raise SystemExit
+    """enters shutdown mode"""
+    _logger.info("receiving shutdown signal now")
+    raise SystemExit
 
 class SerialForwarderTCPServer():
+    """SerialRTU2TCP Forwarder Server"""
+    def __init__(self):
+        self.server=None
+
     async def run(self):
+        """run the server"""
         port, baudrate, server_port, server_ip, slaves = get_commandline()
         client = ModbusSerialClient(method='rtu', port=port, baudrate=baudrate)
-        _logger.info("RTU bus on {} - baudrate {}".format(port, baudrate))
+        _logger.info("RTU bus on %s - baudrate %s", port, baudrate)
         store={}
         for i in slaves:
             store[i]=RemoteSlaveContext(client, unit=i)
         context = ModbusServerContext(slaves=store, single=False)
         self.server = ModbusTcpServer(context, address=(server_ip, server_port), allow_reuse_address=True)
-        _logger.info("serving on {} port {}".format(server_ip, server_port))
-        _logger.info("listening to slaves : {}".format(context.slaves()))
+        _logger.info("serving on %s port %s", server_ip, server_port)
+        _logger.info("listening to slaves : %s", context.slaves())
         await self.server.serve_forever()
 
     async def stop(self):
-        await self.server.shutdown()
-        _logger.info("TCP server is down")
+        """stop the server"""
+        if self.server:
+            await self.server.shutdown()
+            _logger.info("TCP server is down")
 
 def get_commandline():
     """Read and validate command line arguments"""

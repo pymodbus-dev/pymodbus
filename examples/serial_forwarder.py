30477eace8401a7eca1b205642173051f8b3c8ee
Alexandre CUER <alexandre.cuer@cerema.fr>
2022-10-16 17:09:02 +0200

    flake8...

--- examples/serial_forwarder.py
+++ examples/serial_forwarder.py
@@ -19,24 +19,27 @@ FORMAT = "%(asctime)-15s %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s"
 logging.basicConfig(format=FORMAT)
 _logger = logging.getLogger()
 
+
 def raise_graceful_exit(*args):
-    """enters shutdown mode"""
+    """Enters shutdown mode"""
     _logger.info("receiving shutdown signal now")
     raise SystemExit
+
 
 class SerialForwarderTCPServer():
     """SerialRTU2TCP Forwarder Server"""
     def __init__(self):
-        self.server=None
+        """Initialize the server"""
+        self.server = None
 
     async def run(self):
-        """run the server"""
+        """Run the server"""
         port, baudrate, server_port, server_ip, slaves = get_commandline()
         client = ModbusSerialClient(method='rtu', port=port, baudrate=baudrate)
         _logger.info("RTU bus on %s - baudrate %s", port, baudrate)
-        store={}
+        store = {}
         for i in slaves:
-            store[i]=RemoteSlaveContext(client, unit=i)
+            store[i] = RemoteSlaveContext(client, unit=i)
         context = ModbusServerContext(slaves=store, single=False)
         self.server = ModbusTcpServer(context, address=(server_ip, server_port), allow_reuse_address=True)
         _logger.info("serving on %s port %s", server_ip, server_port)
@@ -44,20 +47,21 @@ class SerialForwarderTCPServer():
         await self.server.serve_forever()
 
     async def stop(self):
-        """stop the server"""
+        """Stop the server"""
         if self.server:
             await self.server.shutdown()
             _logger.info("TCP server is down")
 
+
 def get_commandline():
     """Read and validate command line arguments"""
-    logchoices=["critical", "error", "warning", "info", "debug"]
+    logchoices = ["critical", "error", "warning", "info", "debug"]
 
     parser = argparse.ArgumentParser(description="Command line options for examples")
     parser.add_argument("--log", help=",".join(logchoices), default="info", type=str)
-    parser.add_argument("--port", help="RTU serial port", default = '/dev/ttyUSB0', type=str)
-    parser.add_argument("--baudrate", help="RTU baudrate", default = 9600, type=int)
-    parser.add_argument("--server_port", help="server port", default = 5020, type=int)
+    parser.add_argument("--port", help="RTU serial port", default='/dev/ttyUSB0', type=str)
+    parser.add_argument("--baudrate", help="RTU baudrate", default=9600, type=int)
+    parser.add_argument("--server_port", help="server port", default=5020, type=int)
     parser.add_argument("--server_ip", help="server IP", default="127.0.0.1", type=str)
     parser.add_argument("--slaves", help="list of slaves to forward", type=int, nargs="+")
 
@@ -68,6 +72,7 @@ def get_commandline():
     if not args.slaves:
         args.slaves = {1,2,3}
     return args.port, args.baudrate, args.server_port, args.server_ip, args.slaves
+
 
 if __name__ == "__main__":
     server = SerialForwarderTCPServer()

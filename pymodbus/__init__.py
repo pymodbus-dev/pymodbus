2b6173d00d996530219f4aa27b408866584dd3bc
Blaise Thompson <blaise@untzag.com>
2022-10-17 14:39:12 -0500

    migrate function to init

--- pymodbus/__init__.py
+++ pymodbus/__init__.py
@@ -3,12 +3,25 @@
 Released under the the BSD license
 """
 
+import logging as __logging
+from logging import NullHandler as __null
+
 import pymodbus.version as __version
-from ._logging import *
 
 
 __version__ = __version.version.short()
 __author__ = "Galen Collins"
 __maintainer__ = "dhoomakethu"
 
+# ---------------------------------------------------------------------------#
+#  Block unhandled logging
+# ---------------------------------------------------------------------------#
+__logging.getLogger(__name__).addHandler(__null())
 
+
+def pymodbus_apply_logging_config():
+    __logging.basicConfig(
+        format="%(asctime)s %(levelname)-5s %(module)s:%(lineno)s %(message)s",
+        datefmt="%H:%M:%S",
+        level=__logging.WARNING,
+    )

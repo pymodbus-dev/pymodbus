1031299d1f05e8dffb79a9adb72d555c1c05c2e7
wlcrs <wlcrs@users.noreply.github.com>
2023-03-10 08:54:42 +0100

    Organize imports

--- pymodbus/client/base.py
+++ pymodbus/client/base.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 import asyncio
 import socket
 from dataclasses import dataclass
-from typing import Any, Tuple, Type, Callable, Optional
+from typing import Any, Callable, Optional, Tuple, Type
 
 from pymodbus.client.mixin import ModbusClientMixin
 from pymodbus.constants import Defaults

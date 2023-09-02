fb80c1525892edd8b69a0520a029f47e5fca57d0
Logan Gunthorpe <logang@deltatee.com>
2023-09-02 07:45:54 -0600

    transport: Don't raise a RunTimeError in ModbusProtocol.error_received() (#1758)

--- test/sub_transport/test_basic.py
+++ test/sub_transport/test_basic.py
@@ -111,11 +111,6 @@ class TestBasicModbusProtocol:
         """Test eof_received."""
         client.eof_received()
 
-    async def test_error_received(self, client):
-        """Test error_received."""
-        with pytest.raises(RuntimeError):
-            client.error_received(Exception("test call"))
-
     async def test_callbacks(self, use_clc):
         """Test callbacks."""
         client = ModbusProtocol(use_clc, False)

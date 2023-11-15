c88d4b17315b405f88045b0a2adbb4bda0c3017a
Alex Ruddick <a.ruddick@numat-tech.com>
2023-11-15 12:53:47 -0600

    Remove unnecessary abort() call
    Co-authored-by:  kelvin <88643500+garvan2021@users.noreply.github.com>

--- pymodbus/transport/transport_serial.py
+++ pymodbus/transport/transport_serial.py
@@ -117,7 +117,7 @@ class SerialTransport(asyncio.Transport):
         return False
 
     def abort(self) -> None:
-        """Close the transport immediately."""
+        """Old alias for closing the connection."""
         self.close()
 
     # ------------------------------------------------

4c904e80803499a6dd49bc1b6533a3deba9e93b8
jan iversen <jancasacondor@gmail.com>
2023-12-09 18:23:55 +0100

    Prepare for next release.

--- pymodbus/__init__.py
+++ pymodbus/__init__.py
@@ -18,5 +18,5 @@ from pymodbus.logging import pymodbus_apply_logging_config
 from pymodbus.pdu import ExceptionResponse
 
 
-__version__ = "3.6.2"
+__version__ = "3.7.0dev"
 __version_full__ = f"[pymodbus, version {__version__}]"

03277de56b755a84758abeacb8b348fa04fd859b
Philip Couling <couling@gmail.com>
2023-12-16 11:38:27 +0000

    Same problem with http server

--- pymodbus/server/simulator/http_server.py
+++ pymodbus/server/simulator/http_server.py
@@ -115,7 +115,7 @@ class ModbusSimulatorServer:
         self,
         modbus_server: str = "server",
         modbus_device: str = "device",
-        http_host: str = "0.0.0.0",
+        http_host: str | None = None,
         http_port: int = 8080,
         log_file: str = "server.log",
         json_file: str = "setup.json",

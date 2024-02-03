0f11d656cc462cd5291977358460ef96942e0221
jan iversen <jancasacondor@gmail.com>
2024-02-03 22:11:48 +0100

    s

--- pymodbus/transport/stub.py
+++ pymodbus/transport/stub.py
@@ -1,31 +1,21 @@
 """ModbusProtocol network stub."""
 from __future__ import annotations
 
-from pymodbus.transport.transport import CommParams, ModbusProtocol
+from pymodbus.transport.transport import ModbusProtocol
 
 
 class ModbusProtocolStub(ModbusProtocol):
     """Protocol layer including transport."""
 
-    def __init__(
-        self,
-        params: CommParams,
-        is_server: bool,
-    ) -> None:
-        """Initialize Network stub."""
-        super().__init__(params, is_server)
-
     async def start_run(self):
         """Call need functions to start server/client."""
         if  self.is_server:
             return await self.transport_listen()
-        else:
-            return await self.transport_connect()
+        return await self.transport_connect()
 
     def callback_data(self, data: bytes, addr: tuple | None = None) -> int:
         """Handle received data."""
-        response = self.stub_handle_data(data)
-        if response:
+        if (response := self.stub_handle_data(data)):
             self.transport_send(response)
         return len(data)
 
@@ -34,4 +24,6 @@ class ModbusProtocolStub(ModbusProtocol):
     # ---------------- #
     def stub_handle_data(self, data: bytes) -> bytes | None:
         """Handle received data."""
+        if len(data) > 5:
+            return data
         return None

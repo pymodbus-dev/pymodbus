0dd42a5383fb0489a0a22f52bff476f2b2993b0e
jan iversen <jancasacondor@gmail.com>
2024-02-04 15:26:12 +0100

    test combinaations.

--- test/test_network.py
+++ test/test_network.py
@@ -42,20 +42,28 @@ class TestNetwork:
             addr = int(data[9])
             response = data[0:5] + b'\x05\x00\x03\x02\x00' + (addr*10).to_bytes()
 
-            # 1, 4, 8 return correct data
+            # 1, 4, 7 return correct data
             # 2, 5 return NO data
             # 3 return 2 + 3
-            # 6 return 5 + half 6
-            # 7 return second half 6 + 7
-            if addr in (2, 0):
+            # 6 return 6 + 6 (wrong order
+            # 8 return 7 + half 8
+            # 9 return second half 8 + 9
+            if addr in (2, 5):
                 old_data = response
                 response = None
             elif addr == 3:
                 response = old_data + response
-            #elif addr == 6:
-            #    response = old_data
-            #elif addr == 7:
-            #    response = old_data
+                old_data = b''
+            elif addr == 6:
+                response = response + old_data
+                old_data = b''
+            elif addr == 8:
+                x =  response
+                response = response[:7]
+                old_data = x[7:]
+            elif addr == 9:
+                response = old_data + response
+                old_data = b''
             return response
 
         async def local_call(addr: int) -> bool:
@@ -70,6 +78,6 @@ class TestNetwork:
         await stub.start_run()
 
         assert await client.connect()
-        await asyncio.gather(*[local_call(x) for x in range(1, 4)])
+        await asyncio.gather(*[local_call(x) for x in range(1, 10)])
         client.transport_close()
         stub.transport_close()

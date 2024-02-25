b5b11ea0cdf40aa95363b8b74a3329b59c778e83
jan iversen <jancasacondor@gmail.com>
2024-02-25 22:39:52 +0100

    add Message Socket/TLS and amend tests. (#2061)

--- pymodbus/message/socket.py
+++ pymodbus/message/socket.py
@@ -6,6 +6,7 @@ ModbusMessage provides a unified interface to send/receive Modbus requests/respo
 """
 from __future__ import annotations
 
+from pymodbus.logging import Log
 from pymodbus.message.base import MessageBase
 
 
@@ -19,10 +20,26 @@ class MessageSocket(MessageBase):
     * length = uid + function code + data
     """
 
-    def decode(self, _data: bytes) -> tuple[int, int, int, bytes]:
+    def decode(self, data: bytes) -> tuple[int, int, int, bytes]:
         """Decode message."""
-        return 0, 0, 0, b''
+        if (used_len := len(data)) < 9:
+          Log.debug("Very short frame (NO MBAP): {} wait for more data", data, ":hex")
+          return 0, 0, 0, self.EMPTY
+        msg_tid = int.from_bytes(data[0:2], 'big')
+        msg_len = int.from_bytes(data[4:6], 'big') + 6
+        msg_dev = int(data[6])
+        if used_len < msg_len:
+          Log.debug("Short frame: {} wait for more data", data, ":hex")
+          return 0, 0, 0, self.EMPTY
+        return msg_len, msg_tid, msg_dev, data[7:msg_len]
 
-    def encode(self, _data: bytes, _device_id: int, _tid: int) -> bytes:
+    def encode(self, data: bytes, device_id: int, tid: int) -> bytes:
         """Decode message."""
-        return b''
+        packet = (
+           tid.to_bytes(2, 'big') +
+           b'\x00\x00' +
+           (len(data) + 1).to_bytes(2, 'big') +
+           device_id.to_bytes(1, 'big') +
+           data
+        )
+        return packet

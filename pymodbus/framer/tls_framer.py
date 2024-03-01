9cf07243838c12563b2ad710efbb70fb1b0c0959
jan iversen <jancasacondor@gmail.com>
2024-03-01 19:29:47 +0100

    Update framers to ease message integration (only decode/encode) (#2064)

--- pymodbus/framer/tls_framer.py
+++ pymodbus/framer/tls_framer.py
@@ -3,7 +3,6 @@
 import struct
 
 from pymodbus.exceptions import (
-    InvalidMessageReceivedException,
     ModbusIOException,
 )
 from pymodbus.framer.base import TLS_FRAME_HEADER, ModbusFramer
@@ -36,51 +35,6 @@ class ModbusTlsFramer(ModbusFramer):
         super().__init__(decoder, client)
         self._hsize = 0x0
 
-    # ----------------------------------------------------------------------- #
-    # Private Helper Functions
-    # ----------------------------------------------------------------------- #
-    def checkFrame(self):
-        """Check and decode the next frame.
-
-        Return true if we were successful.
-        """
-        if self.isFrameReady():
-            # we have at least a complete message, continue
-            if len(self._buffer) - self._hsize >= 1:
-                return True
-        # we don't have enough of a message yet, wait
-        return False
-
-    def advanceFrame(self):
-        """Skip over the current framed message.
-
-        This allows us to skip over the current message after we have processed
-        it or determined that it contains an error. It also has to reset the
-        current frame header handle
-        """
-        self._buffer = b""
-        self._header = {}
-
-    def isFrameReady(self):
-        """Check if we should continue decode logic.
-
-        This is meant to be used in a while loop in the decoding phase to let
-        the decoder factory know that there is still data in the buffer.
-
-        :returns: True if ready, False otherwise
-        """
-        return len(self._buffer) > self._hsize
-
-    def getFrame(self):
-        """Return the next frame from the buffered data.
-
-        :returns: The next full frame buffer
-        """
-        return self._buffer[self._hsize :]
-
-    # ----------------------------------------------------------------------- #
-    # Public Member Functions
-    # ----------------------------------------------------------------------- #
     def decode_data(self, data):
         """Decode data."""
         if len(data) > self._hsize:
@@ -91,26 +45,31 @@ class ModbusTlsFramer(ModbusFramer):
     def frameProcessIncomingPacket(self, single, callback, slave, _tid=None, **kwargs):
         """Process new packet pattern."""
         # no slave id for Modbus Security Application Protocol
-        if not self.isFrameReady():
+        def check_frame(self):
+            """Check and decode the next frame."""
+            if len(self._buffer) > self._hsize:
+                # we have at least a complete message, continue
+                if len(self._buffer) - self._hsize >= 1:
+                    return True
+            # we don't have enough of a message yet, wait
+            return False
+
+        if not len(self._buffer) > self._hsize:
             return
-        if not self.checkFrame():
+        if not check_frame(self):
             Log.debug("Frame check failed, ignoring!!")
             self.resetFrame()
             return
         if not self._validate_slave_id(slave, single):
             Log.debug("Not in valid slave id - {}, ignoring!!", slave)
             self.resetFrame()
-        self._process(callback)
-
-    def _process(self, callback, error=False):
-        """Process incoming packets irrespective error condition."""
-        data = self._buffer if error else self.getFrame()
+            return
+        data = self._buffer[self._hsize :]
         if (result := self.decoder.decode(data)) is None:
             raise ModbusIOException("Unable to decode request")
-        if error and result.function_code < 0x80:
-            raise InvalidMessageReceivedException(str(result))
         self.populateResult(result)
-        self.advanceFrame()
+        self._buffer = b""
+        self._header = {}
         callback(result)  # defer or push to a thread?
 
     def buildPacket(self, message):

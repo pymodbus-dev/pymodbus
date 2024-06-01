cde272ea4544dff188795cf81925c10e0e6c209d
jan iversen <jancasacondor@gmail.com>
2024-06-01 21:06:36 +0200

    Framer optimization (apart from RTU).

--- pymodbus/framer/base.py
+++ pymodbus/framer/base.py
@@ -6,6 +6,8 @@ According to the selected type of modbus frame a prefix/suffix is added/removed
 """
 from __future__ import annotations
 
+from abc import abstractmethod
+
 
 class FramerBase:
     """Intern base."""
@@ -15,6 +17,13 @@ class FramerBase:
     def __init__(self) -> None:
         """Initialize a ADU instance."""
 
+    def set_dev_ids(self, _dev_ids: list[int]):
+        """Set/update allowed device ids."""
+
+    def set_fc_calc(self, _fc: int, _msg_size: int, _count_pos: int):
+        """Set/Update function code information."""
+
+    @abstractmethod
     def decode(self, data: bytes) -> tuple[int, int, int, bytes]:
         """Decode ADU.
 
@@ -24,12 +33,11 @@ class FramerBase:
             device_id (int) or 0
             modbus request/response (bytes)
         """
-        raise RuntimeError("NOT IMPLEMENTED!")
 
+    @abstractmethod
     def encode(self, pdu: bytes, dev_id: int, tid: int) -> bytes:
         """Encode ADU.
 
         returns:
             modbus ADU (bytes)
         """
-        raise RuntimeError("NOT IMPLEMENTED!")
--- test/framers/conftest.py
+++ test/framers/conftest.py
@@ -5,27 +5,28 @@ from unittest import mock
 
 import pytest
 
+from pymodbus.factory import ClientDecoder, ServerDecoder
 from pymodbus.framer import Framer, FramerType
 from pymodbus.transport import CommParams, ModbusProtocol
 
 
-class DummyMessage(Framer):
+class DummyFramer(Framer):
     """Implement use of ModbusProtocol."""
 
     def __init__(self,
-            message_type: FramerType,
+            framer_type: FramerType,
             params: CommParams,
             is_server: bool,
             device_ids: list[int] | None,
         ):
-        """Initialize a message instance."""
-        super().__init__(message_type, params, is_server, device_ids)
+        """Initialize a frame instance."""
+        super().__init__(framer_type, params, is_server, device_ids)
         self.send = mock.Mock()
-        self.message_type = message_type
+        self.framer_type = framer_type
 
     def callback_new_connection(self) -> ModbusProtocol:
         """Call when listener receive new connection request."""
-        return DummyMessage(self.message_type, self.comm_params, self.is_server, self.device_ids)  # pragma: no cover
+        return DummyFramer(self.framer_type, self.comm_params, self.is_server, self.device_ids)  # pragma: no cover
 
     def callback_connected(self) -> None:
         """Call when connection is succcesfull."""
@@ -37,7 +38,29 @@ class DummyMessage(Framer):
         """Handle received modbus request/response."""
 
 
-@pytest.fixture(name="dummy_message")
-async def prepare_dummy_message():
-    """Return message object."""
-    return DummyMessage
+@pytest.fixture(name="entry")
+def prepare_entry():
+    """Return framer_type."""
+    return FramerType.RAW
+
+@pytest.fixture(name="is_server")
+def prepare_is_server():
+    """Return client/server."""
+    return False
+
+@pytest.fixture(name="dummy_framer")
+async def prepare_test_framer(entry, is_server):
+    """Return framer object."""
+    framer = DummyFramer(
+        entry,
+        CommParams(),
+        is_server,
+        [0, 1],
+    )
+    if entry == FramerType.RTU:
+        func_table = (ServerDecoder if is_server else ClientDecoder)().lookup
+        for key, ent in func_table.items():
+            fix_len = ent._rtu_frame_size if hasattr(ent, "_rtu_frame_size") else 0  # pylint: disable=protected-access
+            cnt_pos = ent. _rtu_byte_count_pos if hasattr(ent, "_rtu_byte_count_pos") else 0  # pylint: disable=protected-access
+            framer.handle.set_fc_calc(key, fix_len, cnt_pos)
+    return framer

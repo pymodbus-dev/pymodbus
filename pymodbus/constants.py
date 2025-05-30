37af0404f728de7ffb70502075c8a3bd82dd4505
jan iversen <jancasacondor@gmail.com>
2025-05-30 15:58:55 +0200

    Remove unused const.

--- pymodbus/constants.py
+++ pymodbus/constants.py
@@ -37,31 +37,6 @@ class ModbusStatus(int, enum.Enum):
     OFF = 0x0000
 
 
-class Endian(str, enum.Enum):
-    """An enumeration representing the various byte endianness.
-
-    .. attribute:: AUTO
-
-       This indicates that the byte order is chosen by the
-       current native environment.
-
-    .. attribute:: BIG
-
-       This indicates that the bytes are in big endian format
-
-    .. attribute:: LITTLE
-
-       This indicates that the bytes are in little endian format
-
-    .. note:: I am simply borrowing the format strings from the
-       python struct module for my convenience.
-    """
-
-    AUTO = "@"
-    BIG = ">"
-    LITTLE = "<"
-
-
 class ModbusPlusOperation(int, enum.Enum):
     """Represents the type of modbus plus request.
 
@@ -127,4 +102,3 @@ class MoreData(int, enum.Enum):
 
     NOTHING = 0x00
     KEEP_READING = 0xFF
-

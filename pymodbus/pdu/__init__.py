527a3054336d4de2ad59546912594391da63412c
jan iversen <jancasacondor@gmail.com>
2025-08-28 19:41:06 +0200

    Framer test 100%. (#2754)

--- pymodbus/pdu/__init__.py
+++ pymodbus/pdu/__init__.py
@@ -1,12 +1,24 @@
 """Framer."""
 __all__ = [
     "DecodePDU",
+    "DiagnosticBase",
     "ExceptionResponse",
     "FileRecord",
     "ModbusPDU",
+    "ReadCoilsRequest",
+    "ReadDeviceInformationRequest",
+    "ReadExceptionStatusRequest",
+    "ReadHoldingRegistersRequest",
 ]
 
+from .bit_message import ReadCoilsRequest
 from .decoders import DecodePDU
+from .diag_message import DiagnosticBase
 from .exceptionresponse import ExceptionResponse
 from .file_message import FileRecord
+from .mei_message import ReadDeviceInformationRequest
+from .other_message import ReadExceptionStatusRequest
 from .pdu import ModbusPDU
+from .register_message import ReadHoldingRegistersRequest
+
+
